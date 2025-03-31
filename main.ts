cb4496c09c463dc79395c89a47378b3c79aa4f94
lisa-vdm-2005 <talyssa.vandemaele@student.vives.be>
2025-03-31 14:33:58 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,21 +3,18 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onSound(DetectedSound.Loud, function () {
     klappen = !(klappen)
-    if (klappen) {
-        start = 1
-    } else {
-    	
-    }
 })
 input.onButtonPressed(Button.B, function () {
     Timer = 180
 })
-let start = 0
 let klappen = false
 let Timer = 0
 input.setSoundThreshold(SoundThreshold.Loud, 128)
 loops.everyInterval(1000, function () {
-    if (start == 1) {
-        basic.showNumber(Timer - 1)
+    if (klappen == true) {
+        Timer += -1
     }
+})
+basic.forever(function () {
+    basic.showNumber(Timer)
 })
