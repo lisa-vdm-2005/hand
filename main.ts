26081b79e12acbc5e81421b4a6bed70f4ff76162
lisa-vdm-2005 <talyssa.vandemaele@student.vives.be>
2025-03-31 14:11:35 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
 	
 })
+input.onSound(DetectedSound.Loud, function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showString("3 2 1")
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showString("5 4 3 2 1")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.setSoundThreshold(SoundThreshold.Loud, 128)
